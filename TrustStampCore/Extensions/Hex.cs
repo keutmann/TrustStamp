4ad73a5f238595b9c175bc3ca94c9d062b354971
keutmann <keutmann@yahoo.com>
2017-01-25 22:22:47 +0100

    Added some core code. Added TrustStampServer code.

---
+++ TrustStampCore/Extensions/Hex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrustStampCore.Extensions
+{
+    public static class Hex
+    {
+        public static byte[] Combine(byte[] left, byte[] right)
+        {
+            var s = new List<byte>(left);
+            s.AddRange(right);
+            return s.ToArray();
+        }
+
+        public static byte[] ToBytes(string hex)
+        {
+            if (hex.Length % 2 == 1)
+                throw new Exception("The binary key cannot have an odd number of digits");
+
+            byte[] arr = new byte[hex.Length >> 1];
+
+            for (int i = 0; i < hex.Length >> 1; ++i)
+            {
+                arr[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+            }
+
+            return arr;
+        }
+
+        public static string ToHex(byte[] data)
+        {
+            return BitConverter.ToString(data).Replace("-", "");
+        }
+
+        public static int GetHexVal(char hex)
+        {
+            int val = (int)hex;
+            //For uppercase A-F letters:
+            return val - (val < 58 ? 48 : 55);
+            //For lowercase a-f letters:
+            //return val - (val < 58 ? 48 : 87);
+            //Or the two combined, but a bit slower:
+            //return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+        }
+    }
+
+}
---
+++ TrustStampCore/Repository/BatchTable.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrustStampCore.Repository
+{
+    public class BatchTable : DBTable
+    {
+
+        public BatchTable(SQLiteConnection connection, string tableName = "Batch")
+        {
+            Connection = connection;
+            TableName = tableName;
+        }
+
+        public void CreateIfNotExist()
+        {
+            string sql = "create table if not exists Batch "+
+                "(id INT PRIMARY KEY NOT NULL,"+
+                "root NVARCHAR(256),"+
+                "state TINYINT,"+
+                "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)";
+            SQLiteCommand command = new SQLiteCommand(sql, Connection);
+            command.ExecuteNonQuery();
+        }
+
+        public JArray GetUnprocessed()
+        {
+            JArray array = new JArray();
+            var sql = "select * from Batch where state = 1 order by Timestamp";
+            var command = new SQLiteCommand(sql, Connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                array.Add(new JObject(
+                    new JProperty("id", reader["id"]),
+                    new JProperty("root", reader["root"]),
+                    new JProperty("state", reader["state"]),
+                    new JProperty("timesamp", reader["timestamp"])
+                    ));
+            }
+
+            return array;
+        }
+    }
+}
