d73afbc98dd15dc0ed9e9d8f02eff0413fc8493d
keutmann <keutmann@yahoo.com>
2017-02-04 21:54:57 +0100

    Added Status ApiController Added ClientDemo application

---
+++ ClientDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TrustStampCore.Service;
+using TrustStampCore.Extensions;
+
+
+namespace ClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 5;
+            using (WebClient client = new WebClient())
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    var id = Crypto.GetRandomHash().ConvertToHex();
+                    var url = "http://127.0.0.1:12700/api/proof/" + id;
+                    Console.WriteLine("Adding proof: "+ id);
+                    client.UploadString(url, id);
+                }
+                Console.WriteLine("Added " + count + " proofs!");
+            }
+
+            Console.WriteLine("Done - Press a key!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TrustStampCore/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TrustStampCore.Repository;
+
+namespace TrustStampCore.Controllers
+{
+    public class StatusController : ApiController
+    {
+
+        public IHttpActionResult GetStatus()
+        {
+            try
+            {
+                using (var db = TimeStampDatabase.Open())
+                {
+                    var status = new JObject();
+                    status["Proof"] = db.ProofTable.Count();
+                    status["Batch"] = db.BatchTable.Count();
+                    return Ok(status);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ExceptionResult(ex, this);
+            }
+        }
+
+
+    }
+}
